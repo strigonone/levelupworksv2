8d19e72e02ad1438e09dd660bb8ece041738b28a
gar212 <gartoyou@gmail.com>
2021-06-21 14:53:03 +1200

    Add switching between login & register

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React,  { useState } from 'react'
 import './LoginForm.css';
-import placeholderimg from '../assets/img/placeholderimg.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
@@ -8,12 +7,14 @@ const LoginForm = (props) => {
 
     // Closes modal if backdrop is clicked
     const backdropClicked = (e) => {if (props.showModal === true && e.target) props.setModal(false)};
+    const [showLogin, setLogin] = useState(true);
 
     return (
     <div className={props.showModal ? "login-form" : "login-form hide" }>
         <div className="modal-backdrop fade-in" onClick={(e) => backdropClicked(e)}></div>
         <div className="modal-form-container">
-            <div className="login-form-box" >
+            
+        {showLogin ? <div className="login-form-box" >
                 <h1>Login</h1>
                 <button className="close" onClick={() => props.setModal(false)}>x</button>
                 <form>
@@ -21,36 +22,39 @@ const LoginForm = (props) => {
                     <input type="password" name="password" placeholder="Password" required />
                     <p className="forgot-password">Forgot Password?</p>
                     <p className="keep-sign-in"><input type="checkbox" />Keep me signed in</p>                             
-                    <button type="submit" className="submit-button">Sign in</button>    
+                    <button type="submit" className="submit-button" >Sign in</button>    
                 </form>
-                    <p className="alt-register">No account yet? <a href="/">Register</a></p>
+                    <p className="alt-register">No account yet? <button className="switch-link" onClick={() => setLogin(false)}>Register</button></p>
                     <p>OR</p>
                 <ul className="social-media-container">
                     <li><button className="facebook"><FontAwesomeIcon icon={faFacebook} /></button></li>
                     <li><button className="twitter"><FontAwesomeIcon icon={faTwitter} /></button></li>
                     <li><button className="google"><FontAwesomeIcon icon={faGoogle} /></button></li>
                 </ul> 
-            </div>
+            </div>:
 
             <div className="register-form login-form-box">
                 <h1>Create a new account</h1>
+                <button className="close" onClick={() => props.setModal(false)}>x</button>
                 <div>
                     <h2>I'm a...</h2>
                     <form action="">
-                        <ul>
-                            <img src={placeholderimg} alt="" />
-                            <img src={placeholderimg} alt="" />
+                        <ul className="register-selector">
+                            <input id="student-option" type="radio" name="register-op" value="student" />
+                            <label className="register-option student-op" htmlFor="student-option"></label>
+                            <input id="teacher-option" type="radio" name="register-op" value="teacher" />
+                            <label className="register-option teacher-op" htmlFor="teacher-option"></label>
                         </ul>
                         <input type="text" name="username" placeholder="Username" required />
                         <input type="email" name="email" placeholder="Email" required />
                         <input type="password" name="password" placeholder="Password" required />
+                        <button type="submit" className="submit-button register-button">Register</button>  
                     </form>
+                    <p className="alt-register">Have an account? <button className="switch-link" onClick={() => setLogin(true)}>Login</button> </p>
                 </div>
-                
             </div>
+            }
         </div>
-
-
     </div>
     )
 }
