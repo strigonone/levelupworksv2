8b6f0d1f61a514a65df2377d08ef5daa5a8a3974
gar212 <gartoyou@gmail.com>
2021-06-22 14:06:46 +1200

    Hide Registration form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,24 +1,19 @@
-import React,  { useState } from 'react'
+import React from 'react'
 import './LoginForm.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const LoginForm = (props) => {
     // Closes modal if backdrop is clicked
     const backdropClicked = (e) => {if (props.showModal === true && e.target) props.setModal(false)};
-    const [showLogin, setLogin] = useState(true);
+    // const [showLogin, setLogin] = useState(true);
     
-    const resetStateHandler = () => {
-        setLogin(true);
-        props.setModal(false);
-    }
+    const resetStateHandler = () => props.setModal(false)
     
     return (
     <div className={props.showModal ? "login-form" : "login-form hide" }>
         <div className="modal-backdrop fade-in" onClick={(e) => backdropClicked(e)}></div>
         <div className="modal-form-container">
             
-        {showLogin ? <div className="login-form-box" >
+        <div className="login-form-box" >
                 <h1>Login</h1>
                 <button className="close" onClick={() => resetStateHandler()}>x</button>
                 <form>
@@ -28,16 +23,16 @@ const LoginForm = (props) => {
                     <p className="keep-sign-in"><input type="checkbox" />Keep me signed in</p>                             
                     <button type="submit" className="submit-button" >Sign in</button>    
                 </form>
-                    <p className="alt-register">No account yet? <button className="switch-link" onClick={() => setLogin(false)}>Register</button></p>
+                    {/* <p className="alt-register">No account yet? <button className="switch-link" onClick={() => setLogin(false)}>Register</button></p>
                     <p>OR</p>
                 <ul className="social-media-container">
                     <li><button className="facebook"><FontAwesomeIcon icon={faFacebook} /></button></li>
                     <li><button className="twitter"><FontAwesomeIcon icon={faTwitter} /></button></li>
                     <li><button className="google"><FontAwesomeIcon icon={faGoogle} /></button></li>
-                </ul> 
-            </div>:
+                </ul>  */}
+            </div>
 
-            <div className="register-form login-form-box">
+            {/* <div className="register-form login-form-box">
                 <h1>Create a new account</h1>
                 <button className="close" onClick={() => resetStateHandler()}>x</button>
                 <div>
@@ -56,8 +51,8 @@ const LoginForm = (props) => {
                     </form>
                     <p className="alt-register">Have an account? <button className="switch-link" onClick={() => setLogin(true)}>Login</button> </p>
                 </div>
-            </div>
-            }
+            </div> */}
+            
         </div>
     </div>
     )
