9d060edc85ccb47db09e4b1a8a43989d8607da17
gar212 <30423580+gar212@users.noreply.github.com>
2021-06-24 05:55:33 +1200

    Add Enrol Page w/ backend func

--- src/pages/Enrolment.js
+++ src/pages/Enrolment.js
@@ -1,39 +1,100 @@
-import React from 'react'
+import React, {useState} from 'react';
+import './Enrolment.css';
+import axios from 'axios';
 
 const Enrolment = () => {
-    return (
-        <div>
-            <h1>Enrolment form</h1>
-            <div>
-                <h2>Select course:</h2>
-                <form action="">
-                    <ul className="register-selector">
-                        <div>
-                            <input id="student-option" type="radio" name="register-op" value="student" />
-                            <label className="register-option student-op" htmlFor="student-option"></label>
-                        </div>
-                        <div>
-                            <input id="teacher-option" type="radio" name="register-op" value="teacher" />
-                            <label className="register-option teacher-op" htmlFor="teacher-option"></label>
-                        </div>
-                    </ul>
-                    <label htmlFor="fullName">Your Full Name*</label><input type="text" name="fullName" required />
-                    <label htmlFor="email">Email Address*</label><input type="email" name="email" required />
-                    <label htmlFor="password">Password*</label><input type="password" name="password" required />
-                    <label htmlFor="childFullName">Child's name*</label><input type="text" name="childFullName" required />
-                    <label htmlFor="childBirthDate">Child's birthdate*</label><input type="password" name="childBirthDate" required />
-                    <label htmlFor="childSchool">Child's school*</label><input type="text" name="childSchool" required />
-                    <div>
-                        <label htmlFor="codeExperience">Does your child have coding experience?</label>
-                        <input type="radio" name="codeExperience" required />
-                    </div>
-                    <div>
-                        <label htmlFor="scratchExperience">Has your child used Scratch before?</label>
-                        <input type="radio" name="scratchExperience" required />
-                    </div>
+    const [enrolData,setEnrol] = useState(
+        {  
+            courseNo: "1",
+            fullName: "",
+            email: "",
+            password: "",
+            childName: "",
+            childBirthDate: "",
+            childSchool: "",
+            childCodingExp: "",
+            childScratchExp: ""
+        }
+    )
 
-                    <button type="submit" className="submit-button register-button">Proceed</button>  
-                </form>
+    const handleEnrolChange = e => {
+        const { name, value } = e.target;
+        setEnrol(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const submitEnrol = e => {
+        e.preventDefault();
+
+        axios.post('http://localhost:6969/api/enrol', enrolData)
+        .then(res => console.log(res.data))
+    }
+    // console.log(enrolData);
+
+    return (
+        <div className="enrol-form-box">
+            <div className="enrol-form-container">
+            <h1>Enrolment form</h1>
+                <div>     
+                    <form onSubmit={submitEnrol}>
+                        <h2>Select course:</h2>
+                        <div className="course-selector">
+                            <label className="course-option">
+                                <h2>Course 1</h2>
+                                <p className="course-desc">Lorem ipsum dolor sit amet</p>
+                                <div className="course-image-placeholder"></div>
+                                <p className="course-small">Start Date: 01 January 2022<br/>Time: 0:00AM<br/>Location: Auckland</p>
+                                <h2 className="course-price">$XXX</h2>
+                                <p className="course-small">for Term 2 (9 Sessions)</p>
+                                <input type="radio" name="courseNo" value="1" checked={enrolData.courseNo === "1"} onChange={handleEnrolChange} />
+                            </label>
+                            <label className="course-option">
+                                <h2>Course 2</h2>
+                                <p className="course-desc">Lorem ipsum dolor sit amet</p>
+                                <div className="course-image-placeholder"></div>
+                                <p className="course-small">Start Date: 01 January 2022<br/>Time: 0:00AM<br/>Location: Auckland</p>
+                                <h2 className="course-price">$XXX</h2>
+                                <p className="course-small">for Term 2 (9 Sessions)</p>
+                                <input type="radio" name="courseNo" value="2" checked={enrolData.courseNo === "2"}  onChange={handleEnrolChange} />
+                            </label>
+                            <label className="course-option">
+                                <h2>Course 3</h2>
+                                <p className="course-desc">Lorem ipsum dolor sit amet</p>
+                                <div className="course-image-placeholder"></div>
+                                <p className="course-small">Start Date: 01 January 2022<br/>Time: 0:00AM<br/>Location: Auckland</p>
+                                <h2 className="course-price">$XXX</h2>
+                                <p className="course-small">for Term 2 (9 Sessions)</p>
+                                <input type="radio" name="courseNo" value="3" checked={enrolData.courseNo === "3"}  onChange={handleEnrolChange} />
+                            </label>
+                        </div>
+
+                        <div className="enrol-field">
+                            <div><label htmlFor="fullName">Your Full Name*</label><input type="text" name="fullName" onChange={handleEnrolChange} value={enrolData.fullName} required /></div>
+                            <div><label htmlFor="email">Email Address*</label><input type="email" name="email" onChange={handleEnrolChange} value={enrolData.email} required /></div>
+                            <div><label htmlFor="password">Password*</label><input type="password" name="password" onChange={handleEnrolChange} value={enrolData.password} required /></div>
+                            <div><label htmlFor="childName">Child's name*</label><input type="text" name="childName" onChange={handleEnrolChange} value={enrolData.childFullName} required /></div>
+                            <div><label htmlFor="childBirthDate">Child's birthdate*</label><input type="text" name="childBirthDate" onChange={handleEnrolChange} value={enrolData.childBirthDate} required /></div>
+                            <div><label htmlFor="childSchool">Child's school*</label><input type="text" name="childSchool" onChange={handleEnrolChange} value={enrolData.childSchool} required /></div>
+                            <div className="radio-selection-container">
+                                <label htmlFor="childCodingExp">Does your child have coding experience?</label>
+                                <div>
+                                    <label>Yes<input type="radio" name="childCodingExp" onChange={handleEnrolChange} value={true} required /></label>
+                                    <label>No<input type="radio" name="childCodingExp" onChange={handleEnrolChange} value={false} required /></label>
+                                </div>
+                            </div>
+                            <div className="radio-selection-container">
+                                <label htmlFor="childScratchExp">Has your child used Scratch before?</label>
+                                <div>
+                                    <label>Yes<input type="radio" name="childScratchExp" onChange={handleEnrolChange} value={true} required /></label>
+                                    <label>No<input type="radio" name="childScratchExp" onChange={handleEnrolChange} value={false} required /></label>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" className="submit-button course-button">Proceed</button>  
+                    </form>
+                </div>
             </div>
         </div>
     )
