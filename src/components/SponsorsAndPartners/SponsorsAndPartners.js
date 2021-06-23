472357ffa25ca8aca9c5e21cc0f76a7b26aeb609
strigonone <s15jaychao@hotmail.com>
2021-06-23 23:27:55 +1200

    Sponsors and Partners content finished

---
+++ src/components/SponsorsAndPartners/SponsorsAndPartners.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./SponsorsAndPartnersStyle.css";
+import GRIDAKL from "../../assets/img/GRIDAKL.jpeg";
+import Tw21 from "../../assets/img/techweek-logo-short-blue.png";
+import NZTech from "../../assets/img/NZTech-300dpi2.png";
+import missionReadyLogo from "../../assets/img/missionReadyLogo.png";
+
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+		padding: theme.spacing(4),
+	},
+	gridImgs: {
+		// textAlign: "center",
+		display: "flex",
+		flexWrap: "wrap",
+		// flexDirection: "row",
+	},
+	img: {
+		margin: "auto",
+		display: "block",
+		maxWidth: "100%",
+		maxHeight: "100%",
+	},
+}));
+
+const SponsorsAndPartners = () => {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.root}>
+			<Container maxWidth="lg">
+				<h1 style={{ textAlign: "center" }}>Sponsors and Partners</h1>
+
+				<Grid
+					item
+					md={12}
+					alignContent="center"
+					direction
+					justify="space-around"
+					className={classes.gridImgs}
+				>
+					<img src={NZTech} alt="NZTech" className={classes.img} />
+					<img
+						src={missionReadyLogo}
+						alt="MissionReadyLogo"
+						className={classes.img}
+					/>
+					<img src={GRIDAKL} alt="gridAkl" className={classes.img} />
+					<img src={Tw21} alt="tw21" className={classes.img} />
+				</Grid>
+			</Container>
+		</div>
+	);
+};
+
+export default SponsorsAndPartners;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import LoginButton from "../components/LoginButton/LoginButton";
 import MainFooter from "../components/MainFooter/MainFooter";
+import SponsorsAndPartners from "../components/SponsorsAndPartners/SponsorsAndPartners";
 
 const HomePage = () => {
 	return (
 		<div>
 			<LoginButton />
+			<SponsorsAndPartners />
 			<MainFooter />
 		</div>
 	);
