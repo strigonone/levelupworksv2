62225ac3787040e6d29e3cf888afa076d5835ff3
gar212 <gartoyou@gmail.com>
2021-06-24 23:43:47 +1200

    Add sign in function

--- src/components/LoginButton/LoginForm.js
+++ src/components/LoginButton/LoginForm.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useState} from 'react';
 import "./LoginForm.css";
+import axios from 'axios';
 
 const LoginForm = (props) => {
 	// Closes modal if backdrop is clicked
 	const backdropClicked = (e) => { if (props.showModal === true && e.target) props.setModal(false);};
 	const resetStateHandler = () => props.setModal(false);
+	const [loginData, setLoginData] = useState(
+        {  
+            email: "",
+            password: "",
+        }
+    )
+
+	const handleLoginChange = e => {
+        const { name, value } = e.target;
+        setLoginData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+	
+    const submitLogin = e => {
+        e.preventDefault();
+        axios.post('http://localhost:6969/api/signin', loginData)
+        .then(res => console.log(res.data))
+    }
 
 	return (
 		<div className={props.showModal ? "login-form" : "login-form hide"}>
@@ -13,9 +34,9 @@ const LoginForm = (props) => {
 				<div className="login-form-box">
 					<h1>Login</h1>
 					<button className="close" onClick={() => resetStateHandler()}>x</button>
-					<form>
-						<input type="text" name="username" placeholder="Username" required />
-						<input type="password" name="password" placeholder="Password" required />
+					<form onSubmit={submitLogin}>
+						<input type="text" name="email" placeholder="Email" onChange={handleLoginChange} value={loginData.email} required />
+						<input type="password" name="password" placeholder="Password" onChange={handleLoginChange} value={loginData.password} required />
 						<p className="forgot-password">Forgot Password?</p>
 						<p className="keep-sign-in"><input type="checkbox" />Keep me signed in</p>
 						<button type="submit" className="submit-button">Log in</button>
